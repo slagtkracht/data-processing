45d359c5484dd1cb43d079a9997dbbb804a52699
slagtkracht <44051626+slagtkracht@users.noreply.github.com>
2018-11-28 00:26:24 +0100

    Add files via upload

---
+++ homework/week_4/d3opdr.js
@@ -0,0 +1,34 @@
+// Rosa Slagt, 11040548
+
+
+            // var tool = d3.select("body")
+                    backround = 
+                    
+
+
+        //     d3.select("body").selectAll("div")
+        //       .data(dataset)
+        //       .enter()
+        //       .append("div")
+        //       .attr("class", "bar");
+
+        //     })
+        // </script>
+
+        //     d3.select("body").selectAll("div")
+        //       .data(dataset)
+        //       .enter()
+        //       .append("div")
+        //       .attr("class", "bar")
+        //       .style("height", function(d) {
+        //           return d + "px";
+        //       });
+                  
+        // <style>           
+        //   div.bar {
+        //     display: inline-block;
+        //     width: 20px;
+        //     height: 75px;
+        //     background-color: teal;
+        //   }
+        // </style>
